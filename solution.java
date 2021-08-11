a22d03fc90ebe13ee0c867b2ba74e83bdd2b5a10
Alan Rodriguez <alanrodriguez102347@gmail.con>
2021-08-11 12:23:14 -0700

    first change

---
+++ solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class solution{
+
+    public static String minWindow(String s, String t){
+        if(s == null || t == null || s.isEmpty() || t.isEmpty()) return "";
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int i = 0, j = 0, left = 0, right = 0;
+        int count = map.size(), min = s.length();
+        boolean found = false;
+
+        while(j < s.length()){
+            char endChar = s.charAt(j++);
+            if(map.containsKey(endChar)){
+                map.put(endChar, map.get(endChar) - 1);
+                if(map.get(endChar) == 0) count -= 1;
+            }
+
+            if(count > 0) continue;
+
+            while(count == 0){
+                char startChar = s.charAt(i++);
+                if(map.containsKey(startChar)){
+                    map.put(startChar, map.get(startChar) + 1);
+                    if(map.get(startChar) > 0) count += 1;
+                }
+            }
+
+            if((j-i) < min){
+                left = i;
+                right = j;
+                min = j - i;
+                found = true;
+            }
+        }
+
+
+        return found? s.substring(left-1, right) : "";
+    }
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC", t = "ABC";
+
+        System.out.println("Final: " + minWindow(s, t));
+    }
+}
+
